09e7021422069f17dce7bebe76b659086b60c7cd
Fernando B. Souza <fernando.bernardes@gmail.com>
2012-10-15 14:45:10 -0300

    Criado o sql connection scope
    Útil para reusar conexões dentro de um bloco "using"

---
+++ Acesso a dados/SqlConnectionScope.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+public class SqlConnectionScope : IDisposable
+{
+    [ThreadStatic]
+    private static int nivel;
+    [ThreadStatic]
+    private static SqlConnection _conexao;
+
+    public SqlConnection CurrentConnection
+    {
+        get { return _conexao; }
+    }
+
+    public SqlConnectionScope(string connectionString)
+    {
+        if (nivel == 0)
+        {
+            _conexao = new SqlConnection(connectionString);
+            _conexao.Open();
+        }
+        nivel++;
+
+    }
+
+    public void Dispose()
+    {
+        nivel--;
+        if (nivel <= 0)
+        {
+            _conexao.Close();
+            _conexao.Dispose();
+            _conexao = null;
+            nivel = 0;
+        }
+    }
+}
