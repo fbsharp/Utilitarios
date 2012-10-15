1391d1f5d6170a53a3ab733c76ea2f0a7159ab70
Fernando B. Souza <fernando.bernardes@gmail.com>
2012-10-15 14:55:09 -0300

    Retirado o namespace

--- Acesso a dados/NHibernateHelper.cs
+++ Acesso a dados/NHibernateHelper.cs
@@ -9,105 +9,102 @@ using NHibernate.Context;
 using System;
 using System.IO;
 
-namespace Concursos.Infra.NH
+public static class NHibernateHelper
 {
-    public static class NHibernateHelper
-    {
-        private static ISessionFactory _sessionFactory;
-        private static object syncRoot = new Object();
+    private static ISessionFactory _sessionFactory;
+    private static object syncRoot = new Object();
 
-        public static ISessionFactory SessionFactory
+    public static ISessionFactory SessionFactory
+    {
+        get
         {
-            get
+            if (_sessionFactory == null)
             {
-                if (_sessionFactory == null)
+                lock (syncRoot)
                 {
-                    lock (syncRoot)
+                    if (_sessionFactory == null)
                     {
-                        if (_sessionFactory == null)
+
+                        System.Runtime.Serialization.IFormatter serializer = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                        NHibernate.Cfg.Configuration cfg = null;
+                        string arquivoComCaminho = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Configuration.serialized");
+
+                        if (File.Exists(arquivoComCaminho))
+                        {
+                            using (Stream stream = File.OpenRead(arquivoComCaminho))
+                            {
+                                cfg = serializer.Deserialize(stream) as Configuration;
+                            }
+                        }
+                        else
                         {
 
-                            System.Runtime.Serialization.IFormatter serializer = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
-                            NHibernate.Cfg.Configuration cfg = null;
-                            string arquivoComCaminho = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Configuration.serialized");
+                            cfg = Fluently.Configure()
+                                .Database(MsSqlConfiguration.MsSql2008
+                                .ConnectionString(c => c.FromConnectionStringWithKey("Conexao")))
+                                .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly())
+                                                               .Conventions.AddFromAssemblyOf<ReferenceConvention>())
+                                //.ExposeConfiguration(BuildSchema)
+                                .ExposeConfiguration(p => p.SetProperty("current_session_context_class", "web"))
+                                .BuildConfiguration();
 
-                            if (File.Exists(arquivoComCaminho))
+                            using (Stream stream = File.OpenWrite(arquivoComCaminho))
                             {
-                                using (Stream stream = File.OpenRead(arquivoComCaminho))
-                                {
-                                    cfg = serializer.Deserialize(stream) as Configuration;
-                                }
+                                serializer.Serialize(stream, cfg);
                             }
-                            else
-                            {
-
-                                cfg = Fluently.Configure()
-                                    .Database(MsSqlConfiguration.MsSql2008
-                                    .ConnectionString(c => c.FromConnectionStringWithKey("Conexao")))
-                                    .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly())
-                                                                   .Conventions.AddFromAssemblyOf<ReferenceConvention>())
-                                    //.ExposeConfiguration(BuildSchema)
-                                    .ExposeConfiguration(p => p.SetProperty("current_session_context_class", "web"))
-                                    .BuildConfiguration();
-
-                                using (Stream stream = File.OpenWrite(arquivoComCaminho))
-                                {
-                                    serializer.Serialize(stream, cfg);
-                                }
-                            }
-
-
-                            _sessionFactory = cfg.BuildSessionFactory();
-                            return _sessionFactory;
                         }
+
+
+                        _sessionFactory = cfg.BuildSessionFactory();
+                        return _sessionFactory;
                     }
                 }
-                return _sessionFactory;
             }
+            return _sessionFactory;
         }
+    }
 
 
 
-        private static void BuildSchema(Configuration config)
+    private static void BuildSchema(Configuration config)
+    {
+        SchemaExport schema = new SchemaExport(config);
+        schema.Drop(false, true);
+        schema.Create(false, true);
+    }
+
+    public static ISession GetCurrentSession()
+    {
+        if (!CurrentSessionContext.HasBind(SessionFactory))
         {
-            SchemaExport schema = new SchemaExport(config);
-            schema.Drop(false, true);
-            schema.Create(false, true);
+            CurrentSessionContext.Bind(SessionFactory.OpenSession());
         }
+        return SessionFactory.GetCurrentSession();
+    }
 
-        public static ISession GetCurrentSession()
-        {
-            if (!CurrentSessionContext.HasBind(SessionFactory))
-            {
-                CurrentSessionContext.Bind(SessionFactory.OpenSession());
-            }
-            return SessionFactory.GetCurrentSession();
-        }
+    public static void DisposeSession()
+    {
+        var session = GetCurrentSession();
+        session.Close();
+        session.Dispose();
+    }
 
-        public static void DisposeSession()
-        {
-            var session = GetCurrentSession();
-            session.Close();
-            session.Dispose();
-        }
+    public static void BeginTransaction()
+    {
+        GetCurrentSession().BeginTransaction();
+    }
 
-        public static void BeginTransaction()
-        {
-            GetCurrentSession().BeginTransaction();
-        }
+    public static void CommitTransaction()
+    {
+        var session = GetCurrentSession();
+        if (session.Transaction.IsActive)
+            session.Transaction.Commit();
+    }
 
-        public static void CommitTransaction()
-        {
-            var session = GetCurrentSession();
-            if (session.Transaction.IsActive)
-                session.Transaction.Commit();
-        }
-
-        public static void RollbackTransaction()
-        {
-            var session = GetCurrentSession();
-            if (session.Transaction.IsActive)
-                session.Transaction.Rollback();
-        }
+    public static void RollbackTransaction()
+    {
+        var session = GetCurrentSession();
+        if (session.Transaction.IsActive)
+            session.Transaction.Rollback();
     }
 }
